c5b99c7f8ff19050de2d1d8346ba71aea60546ff
KMU\icosco <3001day@gmail.com>
2022-06-25 15:53:11 +0900

    환경

---
+++ coffang/src/main/java/com/sparta/coffang/CoffangApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.coffang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffangApplication.class, args);
+    }
+
+}
---
+++ coffang/src/test/java/com/sparta/coffang/CoffangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.coffang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
