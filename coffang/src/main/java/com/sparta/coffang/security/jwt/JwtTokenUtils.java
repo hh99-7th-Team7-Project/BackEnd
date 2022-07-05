0e48f56d43222320cdea71ac2acdb090f5a3c18d
Son-Gyeongi <thsruddl77@gmail.com>
2022-07-06 04:31:39 +0900

    tempSave : token에 user 정보 안전하게  내려줄 수 있다.

--- coffang/src/main/java/com/sparta/coffang/security/jwt/JwtTokenUtils.java
+++ coffang/src/main/java/com/sparta/coffang/security/jwt/JwtTokenUtils.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.sparta.coffang.security.UserDetailsImpl;
 
+import java.util.Collections;
 import java.util.Date;
 
 public final class JwtTokenUtils {
@@ -20,14 +21,20 @@ public final class JwtTokenUtils {
 
     public static final String CLAIM_EXPIRED_DATE = "EXPIRED_DATE";
     public static final String CLAIM_USER_NAME = "USER_NAME";
+    public static final String CLAIM_USER_NICKNAME = "USER_NICKNAME";
+    public static final String CLAIM_USER_IMAGE = "USER_IMAGE";
+    public static final String CLAIM_USER_ROLE = "USER_ROLE";
     public static final String JWT_SECRET = "jwt_secret_!@#$%";
 
     public static String generateJwtToken(UserDetailsImpl userDetails) {
         String token = null;
         try {
             token = JWT.create()
-                    .withIssuer("sparta")
+                    .withIssuer("coffick")
                     .withClaim(CLAIM_USER_NAME, userDetails.getUsername())
+                    .withClaim(CLAIM_USER_NICKNAME, userDetails.getUser().getNickname())
+                    .withClaim(CLAIM_USER_IMAGE, userDetails.getUser().getProfileImage())
+                    .withClaim(CLAIM_USER_ROLE, Collections.singletonList(userDetails.getUser().getRole()))
                      // 토큰 만료 일시 = 현재 시간 + 토큰 유효기간)
                     .withClaim(CLAIM_EXPIRED_DATE, new Date(System.currentTimeMillis() + JWT_TOKEN_VALID_MILLI_SEC))
                     .sign(generateAlgorithm());
