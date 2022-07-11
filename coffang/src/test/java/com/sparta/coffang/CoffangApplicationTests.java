e19edf30db56c2c3c0c57323aaf7c46b96a7188a
Son-Gyeongi <thsruddl77@gmail.com>
2022-07-11 11:27:13 +0900

    edit : test code 주석 처리

--- coffang/src/test/java/com/sparta/coffang/CoffangApplicationTests.java
+++ coffang/src/test/java/com/sparta/coffang/CoffangApplicationTests.java
@@ -3,11 +3,11 @@ package com.sparta.coffang;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CoffangApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
