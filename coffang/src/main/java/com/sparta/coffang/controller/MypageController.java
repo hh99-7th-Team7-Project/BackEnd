0b3ad172c3ad886af754685a089c674ff9bfb9c7
Son-Gyeongi <thsruddl77@gmail.com>
2022-07-11 17:48:10 +0900

    edit : 불필요한 API 지우기

--- coffang/src/main/java/com/sparta/coffang/controller/MypageController.java
+++ coffang/src/main/java/com/sparta/coffang/controller/MypageController.java
@@ -29,34 +29,7 @@ public class MypageController {
 
         return mypageService.updateUser(userId, requestDto, userDetails);
     }
-
-    //유저 이미지 프로필 변경 /formdata형식
-    @PutMapping("/mypage/userInfo/image/{userId}")
-    public ResponseEntity updateUserImage (@PathVariable Long userId,
-                                           @RequestPart("profileImage") List<MultipartFile> profileImages,
-                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
-        String defaultImg = "https://coffang-jun.s3.ap-northeast-2.amazonaws.com/fbcebde7-ae14-42f0-9a75-261914c1053f.png";
-        String image = "";
-        // 이미지를 안 넣으면 기본이미지 주기
-        if(profileImages.get(0).isEmpty()) { //이미지가 안들어오면 true
-            image = defaultImg;
-        } else {  //profileImages에 유저가 등록한 이미지가 들어올 때
-            List<PhotoDto> photoDtos = s3Service.uploadFile(profileImages);
-            image = photoDtos.get(0).getPath();
-        }
-
-        return mypageService.updateUserImage(userId, defaultImg, image, userDetails);
-    }
-
-    //유저 name 프로필 변경 /JSON형식
-    @PutMapping("/mypage/userInfo/name/{userId}")
-    public ResponseEntity updateUserName (@PathVariable Long userId,
-                                          @RequestBody MypageRequestDto requestDto,
-                                          @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
-        return mypageService.updateUserName(userId, requestDto, userDetails);
-    }
+    
 
     // 유저 정보 조회 (username, nickname, profileImage)
     @GetMapping("/mypage/userInfo/{userId}")
