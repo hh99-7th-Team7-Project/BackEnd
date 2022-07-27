9d3a216ed0edca55e4d7973460272060b3e27645
황현준 <104722681+jinlobil@users.noreply.github.com>
2022-07-28 02:43:37 +0900

    Update start.sh

--- coffang/scripts/start.sh
+++ coffang/scripts/start.sh
@@ -27,6 +27,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties\,/home/ubuntu/coffang/application-secret.properties \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties\,/home/ubuntu/coffang/application-$IDLE_PROFILE.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup$IDLE_PROFILE.out 2>&1 &
