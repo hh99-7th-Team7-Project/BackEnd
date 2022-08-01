ede9f7549b8266e60ceb4809f79dd78b8a68145f
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2022-08-02 02:09:31 +0900

    Update run_new_was.sh

--- coffang/scripts/run_new_was.sh
+++ coffang/scripts/run_new_was.sh
@@ -21,8 +21,10 @@ if [ ! -z ${TARGET_PID} ]; then
   echo "> Kill WAS running at ${TARGET_PORT}."
   sudo kill ${TARGET_PID}
 fi
+sleep 5
 
 nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/coffang/deploy/*.jar > /home/ubuntu/nohup.out 2>&1 &
 # nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/coffang/deploy/coffang-0.0.1-SNAPSHOT.jar > /home/ubuntu/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
+sleep 5
 exit 0
